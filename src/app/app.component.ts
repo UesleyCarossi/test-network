99bc776fe4002a93cdfc9cfbcaa6b8e421d22e92
Uesley Carossi <uesleycarossi@sysmo.com.br>
2020-05-27 16:27:38 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Network } from '@ionic-native/network/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private network: Network
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.eventoWifi();
     });
+
+  }
+
+  eventoWifi() {
+
+    this.network.onConnect().subscribe(() => {
+      alert('onConnect');
+    });
+
   }
 }
